0020ef695939e0013afcb773f1a80846e2067329
Debanjan <imdeghosh@gmail.com>
2020-06-17 10:15:11 -0400

    Committing master branch

---
+++ src/test/java/testGit/MergeConflict/File_A.java
@@ -0,0 +1,15 @@
+package testGit.MergeConflict;
+
+import org.testng.annotations.Test;
+
+public class File_A {
+	
+	@Test
+	public void method1()
+	{
+		
+	System.out.println("Print method1");	
+		
+	}
+
+}
