e9ad051e617c0a37cc42bacf4b68da37c2df4030
Debanjan <imdeghosh@gmail.com>
2020-06-17 13:13:47 -0400

    Pulling to Branch1

---
+++ src/test/java/testGit/MergeConflict/File_B.java
@@ -0,0 +1,15 @@
+package testGit.MergeConflict;
+
+import org.testng.annotations.Test;
+
+public class File_B {
+	
+	@Test
+	public void method2()
+	{
+		
+	System.out.println("Print method2");	
+		
+	}
+
+}
---
+++ src/test/java/testGit/MergeConflict/File_C.java
@@ -0,0 +1,15 @@
+package testGit.MergeConflict;
+
+import org.testng.annotations.Test;
+
+public class File_C {
+	
+	@Test
+	public void method3()
+	{
+		
+	System.out.println("Print method3");	
+		
+	}
+
+}
