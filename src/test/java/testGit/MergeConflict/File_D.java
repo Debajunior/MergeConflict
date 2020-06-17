93c0d60429e23059494aff8dcd6b8c0be46f26ee
Debanjan <imdeghosh@gmail.com>
2020-06-17 14:59:58 -0400

    Branch2 commit

---
+++ src/test/java/testGit/MergeConflict/File_D.java
@@ -0,0 +1,15 @@
+package testGit.MergeConflict;
+
+import org.testng.annotations.Test;
+
+public class File_D {
+	
+	@Test
+	public void method4()
+	{
+		
+	System.out.println("Print method4");	
+		
+	}
+
+}
---
+++ src/test/java/testGit/MergeConflict/File_E.java
@@ -0,0 +1,15 @@
+package testGit.MergeConflict;
+
+import org.testng.annotations.Test;
+
+public class File_E {
+	
+	@Test
+	public void method5()
+	{
+		
+	System.out.println("Print method5");	
+		
+	}
+
+}
